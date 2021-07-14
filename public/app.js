b59278c36c8dec7491209a15a6192c4dbe57dbd0
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2021-07-14 14:13:51 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -7,3 +7,30 @@ const btnNew = document.getElementById('btn-new');
 const btnRoll = document.getElementById('btn-roll');
 const btnHold = document.getElementById('btn-hold');
 
+const playersScores = [0, 0];
+let currentScore = 0;
+let currentPlayer = 0;
+
+// Roll the dice
+function roll() {
+
+}
+
+// Hold current score
+function hold() {
+
+}
+
+// Start a new game
+function newGame() {
+
+}
+
+// Switch to next player
+function nextPlayer() {
+
+}
+
+btnRoll.addEventListener('click', roll);
+btnHold.addEventListener('click', hold);
+btnHold.addEventListener('click', newGame);
