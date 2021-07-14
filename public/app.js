c2f0c095d23b70048c331dc41669e3402d27bf15
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2021-07-14 14:12:56 +0200

    Update app.js

---
+++ public/app.js
@@ -0,0 +1,9 @@
+const player1 = document.getElementById('player-0');
+const player2 = document.getElementById('player-1');
+const playerScore1 = document.getElementById('score-0');
+const playerScore2 = document.getElementById('score-1');
+const dice = document.getElementById('dice');
+const btnNew = document.getElementById('btn-new');
+const btnRoll = document.getElementById('btn-roll');
+const btnHold = document.getElementById('btn-hold');
+
